94fd118bc10641f05e986b8d0072f77b93211dec
Ruchira Dasanayake <drl.dasanayake.official@gmail.com>
2024-04-09 22:02:27 +0530

    no message

--- FrontEnd/react-frontend/src/App.tsx
+++ FrontEnd/react-frontend/src/App.tsx
@@ -7,16 +7,11 @@ import { CreateGigForm1 } from './layouts/FreelancerWorkManagement/GigManagement
 import CreateGigForm2 from './layouts/FreelancerWorkManagement/GigManagement/Components/CreateGigForm2';
 import CreateGigForm3 from './layouts/FreelancerWorkManagement/GigManagement/Components/CreateGigForm3';
 import DetailedGigs from './layouts/FreelancerWorkManagement/GigOrder/DetailedGigs';
-import { Footer } from './layouts/navbar&footers/Footer';
-import { Dashboard } from './layouts/Dashboard/dashboard';
-import { Navbar } from './layouts/navbar&footers/Navbar';
-import UploadedImages from './layouts/FreelancerWorkManagement/GigOrder/UploadedImages';
 
 export const App = () => {
   return (
     <Router>
       <div>
-        <Navbar />
         <Switch>
           <Route path="/CreateGigForm1" component={CreateGigForm1} />
           <Route path="/CreateGigForm2/:gigId" component={CreateGigForm2} />
@@ -26,10 +21,7 @@ export const App = () => {
           <Route path="/FreelancerDashboard" component={FreelancerDashboard} />
           <Route path="/edit/:id" component={EditGig} />
           <Route path="/uploadedImages/:gigId" />
-          <Route path="dashboard" component={Dashboard} />
-          <Route path="/uploadedImages"component={UploadedImages} />
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
