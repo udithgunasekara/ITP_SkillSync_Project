d629f01375d92ff32aaf54d89d60b9574136cad3
Ruchira Dasanayake <drl.dasanayake.official@gmail.com>
2024-04-09 19:30:07 +0530

    Uploaded frontend

--- FrontEnd/react-frontend/src/App.tsx
+++ FrontEnd/react-frontend/src/App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Navbar } from './layouts/navbar&footers/Navbar';
-import { Dashboard } from './layouts/Dashboard/dashboard';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EditGig from './layouts/FreelancerWorkManagement/FreelancerDashboard/Components/EditGig';
+import FreelancerDashboard from './layouts/FreelancerWorkManagement/FreelancerDashboard/FreelancerDashboard';
+import FreelancerHome from './layouts/FreelancerWorkManagement/FreelancerHome/FreelancerHome';
+import { CreateGigForm1 } from './layouts/FreelancerWorkManagement/GigManagement/Components/CreateGigForm1';
+import CreateGigForm2 from './layouts/FreelancerWorkManagement/GigManagement/Components/CreateGigForm2';
+import CreateGigForm3 from './layouts/FreelancerWorkManagement/GigManagement/Components/CreateGigForm3';
+import DetailedGigs from './layouts/FreelancerWorkManagement/GigOrder/DetailedGigs';
 import { Footer } from './layouts/navbar&footers/Footer';
+import { Dashboard } from './layouts/Dashboard/dashboard';
+import { Navbar } from './layouts/navbar&footers/Navbar';
 
 export const App = () => {
   return (
-    <>
-    <Navbar/>
-    <Dashboard/>
-    <Footer/>
-    </>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/CreateGigForm1" component={CreateGigForm1} />
+          <Route path="/CreateGigForm2/:gigId" component={CreateGigForm2} />
+          <Route path="/CreateGigForm3/:gigId" component={CreateGigForm3} />
+          <Route path="/FreelancerMain" exact component={FreelancerHome} />
+          <Route path="/gig/:id" component={DetailedGigs} />
+          <Route path="/FreelancerDashboard" component={FreelancerDashboard} />
+          <Route path="/edit/:id" component={EditGig} />
+          <Route path="/uploadedImages/:gigId" />
+          <Route path="dashboard" component={Dashboard} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
+export default App;
