dbc646de6894d0c15e1f3e3892ec0afadf8393e6
Dilshan Gamage <dilshangamage107@gmail.com>
2024-04-16 00:36:36 +0530

    add and changed ticketrepo and public notice repo.. created ticketresponserepo

--- BackEnd/src/main/java/BackEnd/repository/publicNotcesRepo.java
+++ BackEnd/src/main/java/BackEnd/repository/publicNotcesRepo.java
@@ -1,14 +1,18 @@
 package BackEnd.repository;
 
 import BackEnd.entity.publicNotices;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface publicNotcesRepo extends JpaRepository<publicNotices,Long>{
 
-    @Query(value = "select * from `public notices` where audience = :audience",nativeQuery = true)
-    List<publicNotices> findByAudience(String audience);
+    Page<publicNotices> findByTitleContainingOrDescriptionContaining(@RequestParam("title") String title, @RequestParam("description") String description, Pageable pageable);
+
+    Page<publicNotices> findByAudience(@RequestParam("audience")String audience,Pageable pageable);
 
 }
