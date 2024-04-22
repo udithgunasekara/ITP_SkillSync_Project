9f4af89c90deeb731ccd80b060c306e8e1803d34
Dilshan Gamage <dilshangamage107@gmail.com>
2024-04-22 21:35:32 +0530

    removed node modules form version control

--- FrontEnd/react-frontend/src/layouts/AdminTaskManagement/HelpDesk/Helpdesk.tsx
+++
@@ -1,8 +0,0 @@
-import { SupportNavBar } from "./Components/SupportNavBar";
-import './Components/supportnavbarstyle.css';
-
-export const Helpdesk = () => {
-    return(
-        <SupportNavBar/>
-    );
-}
